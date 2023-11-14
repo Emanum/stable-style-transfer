2265f90dab51b12ac29784156f479fbaf3b1dac5
Manuel Eiweck <manuel.eiweck@bkf.at>
2023-11-14 23:43:17 +0100

    add control_net demo with canny filter

--- diffuser-pipeline/style-transfer.py
+++ diffuser-pipeline/style-transfer.py
@@ -1,5 +1,6 @@
 from huggingface_hub import snapshot_download
 from diffusers import DiffusionPipeline as StableDiffusionPipeline
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
 import torch
 from diffusers.utils import load_image
 from PIL import Image
@@ -14,15 +15,32 @@ import numpy as np
 #         "runwayml/stable-diffusion-v1-5", ignore_patterns=ignore
 #     )
 
-def canny(imgfile, low_threshold=100, high_threshold=200):
-    image = load_image(imgfile)
-
+def canny(img, low_threshold=100, high_threshold=200):
+    image = load_image(img)
     image = np.array(image)
 
     image = cv2.Canny(image, low_threshold, high_threshold)
     image = image[:, :, None]
     image = np.concatenate([image, image, image], axis=2)
     return Image.fromarray(image)
+
+
+def control_net(img):
+    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16,
+                                                 use_safetensors=True)
+    pipe = StableDiffusionControlNetPipeline.from_pretrained(
+        "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16, use_safetensors=True
+    ).to("cuda")
+
+    pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe.enable_model_cpu_offload()
+
+    canny_image = canny(img=img, low_threshold=100, high_threshold=200)
+
+    image = pipe(
+        "the mona lisa", image=canny_image
+    ).images[0]
+    image.save("mona_lisa.png")
 
 
 def main():
@@ -36,4 +54,5 @@ def main():
     image.save("astronaut_rides_horse.png")
 
 
-canny("../testData/input_image_vermeer.png")
+# canny("../testData/input_image_vermeer.png")
+control_net("../testData/input_image_vermeer.png")
